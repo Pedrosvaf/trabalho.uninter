5f411b9576079ef0efcffb1795053353f778a7a2
Pedrosvaf <pedrosilvaflores.contato@gmail.com>
2025-07-07 08:55:10 -0300

    Commit inicial

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var al = document.getElementById("mensagem")
+al.onclick = function(){
+  this.remove()
+  alert("O sucesso é a soma de pequenos esforços repetidos dia após dia.")
+  motivacao.removeAttibute("style")
+
+}
